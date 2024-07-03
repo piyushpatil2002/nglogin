60d752a1924eff70a4c375855901e4a270b31c2f
piyushpatil2002 <piyushpatil2002@gmail.com>
2024-07-03 10:14:47 +0530

    final

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,46 +1,54 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import {GetapiService} from '../services/getapi.service'
+import { GetapiService } from '../services/getapi.service'
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage  {
-  email:string=''
+export class LoginPage {
+  email: string = ''
   password: string = '';
-  isLoginError: boolean = false;
-  authError:string=''
   loginForm = new FormGroup({
-          email:new FormControl('',[Validators.required,Validators.email]),
-          password:new FormControl('',[Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
   })
-  constructor(private route: Router,private api: GetapiService){ 
+  constructor(private route: Router, private api: GetapiService) {
 
   }
-  
-  async login(){
-    let formData=this.loginForm.value
-    
+
+  async login() {
+    let formData = this.loginForm.value
+
     // console.log(data)
     // this.route.navigate(['/tabs']);
-    let response:any = await this.api.userlogin(`userLogin?userEmail=${formData.email}&password=${formData.password}`)
+    if (this.loginForm.valid) {
+      let response: any = await this.api.userlogin(`userLogin?userEmail=${formData.email}&password=${formData.password}`)
 
-    if(response  && response.body.length){
-      console.warn("User Logged In")
-      this.route.navigate(['/tabs'])
+      if (response && response.body.length) {
+        console.warn("User Logged In")
+        this.route.navigate(['/tabs'])
+      }
+      else{
+        console.warn("Login Failed")
+        await this.api.presentToast({
+          message: 'Invalid user!',
+          duration: 3000,
+          position: 'top' });
+      }
     }
-    else{
-      console.warn("Login Failed")  
-      this.isLoginError=true
-      this.authError="Invalid User"
+
+    else {
+      console.warn("Login Failed")
+
       await this.api.presentToast({
-        message: 'Invalid user!',
+        message: 'Fill the form properly',
         duration: 3000,
-        position: 'top'
-      });
-      
+        position: 'top' });
     }
+
+
+
   }
 }
